1bd971fd4548ce62464d649526f0eb467d5fbcd5
Fabricio Raphael <fabricioraphael.b@gmail.com>
2021-12-10 17:44:50 -0300

    add project + build script to github actions

---
+++ src/main/java/br/com/devtime/sbgactions/sbgactions/SbgactionsApplication.java
@@ -0,0 +1,13 @@
+package br.com.devtime.sbgactions.sbgactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbgactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbgactionsApplication.class, args);
+	}
+
+}
